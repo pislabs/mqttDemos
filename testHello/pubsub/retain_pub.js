c615812f628e58df0bf826b1fede37a58a2d628d
rayl <liuyi86@zto.com>
2025-11-20 18:13:59 +0800

    feat: 发布订阅reatin消息

---
+++ testHello/pubsub/retain_pub.js
@@ -0,0 +1,30 @@
+const mqtt = require("mqtt");
+
+const serverHost = "mqtt://127.0.0.1:1883";
+
+const client = mqtt.connect(serverHost, {
+  clientId: "mqtt_sample_publisher_1",
+  clean: false,
+});
+
+console.log(`正在连接...${serverHost}`);
+
+client.on("connect", (connack) => {
+  if (connack.returnCode == 0) {
+    client.publish(
+      "home/2ndfloor/201/temperature",
+      JSON.stringify({ current: 25 }),
+      { qos: 0, retain: 1 },
+      (err) => {
+        if (err == undefined) {
+          console.log("Publish finished");
+          client.end();
+        } else {
+          console.log("Publish failed");
+        }
+      }
+    );
+  } else {
+    console.log("Connection failed: ${connack.returnCode}");
+  }
+});
