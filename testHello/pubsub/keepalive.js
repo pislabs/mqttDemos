b1565d90cceca7087a446dd185e19992eb7e4698
rayl <liuyi86@zto.com>
2025-11-21 11:06:52 +0800

    feat: 监控keepalive

---
+++ testHello/pubsub/keepalive.js
@@ -0,0 +1,21 @@
+const mqtt = require("mqtt");
+const dateTime = require("node-datetime");
+
+const serverHost = "mqtt://127.0.0.1:1883";
+console.log(`正在连接...${serverHost}`);
+
+const client = mqtt.connect(serverHost, {
+  clientId: "mqtt_sample_id_chapter_9",
+  clean: false,
+  keepalive: 5,
+});
+
+client.on("connect", function () {
+  client.on("packetsend", function (packet) {
+    console.log(`${dateTime.create().format("H:M:S")}: send ${packet.cmd}`);
+  });
+
+  client.on("packetreceive", function (packet) {
+    console.log(`${dateTime.create().format("H:M:S")}: receive ${packet.cmd}`);
+  });
+});
